ac260d23da81dfe8dff37ed75ec5d4dd78eefb15
index <81807841+twhlynch@users.noreply.github.com>
2024-01-10 00:36:26 +1100

    Update app.py

--- app.py
+++ app.py
@@ -5,28 +5,13 @@ intents = discord.Intents.default()
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 
-log_channel_id = 1115066642741870662
-
 @client.event
 async def on_ready():
     await tree.sync(guild=discord.Object(id=1023949212670509076))
-    print("Ready!")
     
 @client.event
 async def on_message(message):
     if message.channel.id == 1023950794543878205 and "grabvr.quest" not in message.content:
         await message.delete()
-
-    await client.process_commands(message)
-
-@tree.command(
-    name='test',
-    description='Clear level submission',
-    guild=discord.Object(id=1023949212670509076)
-)
-async def test(interaction: discord.Interaction):
-    user_id = interaction.user.id
-
-    await interaction.response.send_message(user_id)
 
 client.run(os.environ.get("bot"))
