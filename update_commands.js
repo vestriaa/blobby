d95a585813564d215462dbf40207433f884dc5e8
Tom Lynch <twhlynch@gmail.com>
2024-03-30 17:31:48 +1100

    c

---
+++ update_commands.js
@@ -0,0 +1,28 @@
+import fetch from 'node-fetch';
+
+const COMMANDS = require('./commands.json');
+const token = process.env.DISCORD_TOKEN;
+const applicationId = process.env.DISCORD_APPLICATION_ID;
+
+async function registerCommands() {
+    const url = `https://discord.com/api/v10/applications/${applicationId}/commands`;
+    const response = await fetch(url, {
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bot ${token}`
+        },
+        method: 'PUT',
+        body: JSON.stringify(COMMANDS)
+    });
+
+    if (response.ok) {
+        console.log('Registered commands');
+    } else {
+        console.error('Error registering commands');
+        const text = await response.text();
+        console.error(text);
+    }
+    return response;
+}
+
+await registerCommands();
