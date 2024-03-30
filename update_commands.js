48dd377dc79aae1bc015f827a03b53d1672bc6aa
Tom Lynch <twhlynch@gmail.com>
2024-03-30 18:29:01 +1100

    maybe

--- update_commands.js
+++ update_commands.js
@@ -4,9 +4,11 @@ import fs from 'fs';
 const commands = JSON.parse(fs.readFileSync('commands.json', 'utf8'));
 const token = process.env.DISCORD_TOKEN;
 const applicationId = process.env.DISCORD_APPLICATION_ID;
+const guildId = "1048213818775437394";
 
 async function registerCommands() {
-    const url = `https://discord.com/api/v10/applications/${applicationId}/commands`;
+    const url = `https://discord.com/api/v10/applications/${applicationId}//commands`;
+    // const url = `https://discord.com/api/v10/applications/${applicationId}/guilds/${guildId}/commands`;
     const response = await fetch(url, {
         headers: {
             'Content-Type': 'application/json',
