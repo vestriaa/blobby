a5bba0760ffd9b6c2d2fd44e0425b0af754a257a
Tom Lynch <twhlynch@gmail.com>
2024-03-30 21:03:44 +1100

    idk

--- worker.js
+++ worker.js
@@ -3,22 +3,26 @@ import { verifyKey } from 'discord-interactions';
 export default {
     async fetch(request, env, ctx) {
 
-        const signature = request.headers["X-Signature-Ed25519"];
-        const timestamp = request.headers["X-Signature-Timestamp"];
+        const signature = request.headers["x-signature-ed25519"];
+        const timestamp = request.headers["x-signature-timestamp"];
+        console.log(signature, timestamp);
         const body = request.rawBody;
 
         const isVerified = signature && timestamp && verifyKey(body, signature, timestamp, env.PUBLIC_KEY);
 
         if (!isVerified) {
+            console.log("invalid request signature");
             return new Response("invalid request signature", {status: 401});
         }
 
         if (request.json?.type == 1) {
+            console.log("PONG");
             return Response.json({
                 type: 1
             });
         }
         
+        console.log("Command")
         return new Response('Hello World!');
 
     },
