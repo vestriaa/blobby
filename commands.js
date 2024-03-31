ec5af87c75473d92449c5a24c0999f21e2aaad75
index <81807841+twhlynch@users.noreply.github.com>
2024-03-31 14:46:43 +1100

    Update commands.js

--- commands.js
+++ commands.js
@@ -22,5 +22,84 @@ export const commands = [
     {
         name: "globalstats",
         description: "Get global level statistics",
+    },
+    {
+        name: "actualtrending",
+        description: "Get the current trending levels list (but better)",
+    },
+    {
+        name: "player",
+        description: "Get a players details and stats",
+        options: [
+            {
+                name: “user_name”,
+                required: true,
+                type: 3
+            }
+        ]
+    },
+    {
+        name: "random",
+        description: "Get a random level",
+        options: [
+            {
+                name: “verified”,
+                required: true,
+                type: 5
+            }
+        ]
+    },
+    {
+        name: "leaderboard",
+        description: "Get a levels leaderboard",
+        options: [
+            {
+                name: “title”,
+                required: true,
+                type: 3
+            },
+            {
+                name: “creator”,
+                required: true,
+                type: 3
+            }
+    
+        ]
+    },
+    {
+        name: "hardest",
+        description: "Hardest maps list functionality",
+        options: [
+            {
+                name: “command”,
+                required: true,
+                type: 3,
+                choices: [
+                    {
+                        name: “list”
+                    },
+                    {
+                        name: “top”
+                    },
+                    {
+                        name: “add”
+                    },
+                    {
+                        name: “remove”
+                    },
+                    {
+                        name: “promote”
+                    },
+                    {
+                        name: “demote”
+                    }
+                ]
+            },
+            {
+                name: “link”,
+                required: false,
+                type: 3
+            }
+        ]
     }
 ];
