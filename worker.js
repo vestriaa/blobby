b745c5cf2291f16fe3e29ab7f0fab7019752cd12
Tom Lynch <twhlynch@gmail.com>
2024-03-31 16:22:14 +1100

    ah

--- worker.js
+++ worker.js
@@ -30,7 +30,7 @@ export default {
 
     formatTime(seconds) {
         let minutes = Math.floor(seconds / 60);
-        let seconds = (seconds % 60).toFixed(maxDecimals);
+        seconds = (seconds % 60).toFixed(maxDecimals);
         if (minutes < 10) { minutes = "0" + minutes; }
         if (seconds < 10) { seconds = "0" + seconds; }
         return `${minutes}:${seconds}`;
