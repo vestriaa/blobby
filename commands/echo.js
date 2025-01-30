b41614fd0314b1337e9bcf9f8d878963bd7dd90b
Tom Lynch <twhlynch@gmail.com>
2025-01-31 00:53:47 +1100

    dont allow echoing mentions

--- commands/echo.js
+++ commands/echo.js
@@ -1,5 +1,16 @@
 export async function echo(json, env) {
     const text = json.data.options[0].value;
+    if (text.includes('@')) {
+        return Response.json({
+            type: 4,
+            data: {
+                tts: false,
+                content: ":3",
+                embeds: [],
+                allowed_mentions: { parse: [] }
+            }
+        });
+    }
     return Response.json({
         type: 4,
         data: {
