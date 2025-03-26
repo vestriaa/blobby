ed6279bdea9fe6e79011207ffa6e1a8e49c40644
Tom Lynch <twhlynch@gmail.com>
2025-03-26 19:48:00 +1100

    whoops, comitted too fast

--- commands/checkStolen.js
+++ commands/checkStolen.js
@@ -4,11 +4,11 @@ export async function player(json, env) {
     const id1 = json.data.options[0].value;
     const id2 = json.data.options[1].value;
 
-    const user1Search = `${CONFIG.API_URL}list?max_format_version=${CONFIG.FORMAT_VERSION}&user_id=${userID}`;
+    const user1Search = `${CONFIG.API_URL}list?max_format_version=${CONFIG.FORMAT_VERSION}&user_id=${id1}`;
     const user1Response = await fetch(user1Search);
     const user1Data = await user1Response.json();
 
-    const user2Search = `${CONFIG.API_URL}list?max_format_version=${CONFIG.FORMAT_VERSION}&user_id=${userID}`;
+    const user2Search = `${CONFIG.API_URL}list?max_format_version=${CONFIG.FORMAT_VERSION}&user_id=${id2}`;
     const user2Response = await fetch(user2Search);
     const user2Data = await user2Response.json();
 
@@ -49,11 +49,7 @@ export async function player(json, env) {
                     "name": "",
                     "value": CONFIG.LEVEL_URL + o[0] + "\n" + CONFIG.LEVEL_URL + o[1],
                     "inline": false
-                }}),
-                "url": creatorUrl,
-                "footer": {
-                    "text": roleString
-                }
+                }})
             }],
             allowed_mentions: { parse: [] }
         }
